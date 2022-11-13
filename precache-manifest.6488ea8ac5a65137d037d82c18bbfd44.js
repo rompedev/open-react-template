ef69cd281801330971f71dba49f4765981073cd8
Yeison Garzón T <toxiicsa@hotmail.com>
2022-11-12 23:35:02 -0500

    Updates

---
+++ precache-manifest.6488ea8ac5a65137d037d82c18bbfd44.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e9808cfe22e8325612a5f2b6c697e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e07e0eeab6d96ed7eb63",
+    "url": "/static/css/main.c43a4c00.chunk.css"
+  },
+  {
+    "revision": "4540f1453b187bbcddb8",
+    "url": "/static/js/2.1d25a979.chunk.js"
+  },
+  {
+    "revision": "d5b473ff893d6b5fac7d0b5081a5072a",
+    "url": "/static/js/2.1d25a979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07e0eeab6d96ed7eb63",
+    "url": "/static/js/main.c540401e.chunk.js"
+  },
+  {
+    "revision": "f1a256b8ca68736efd22",
+    "url": "/static/js/runtime-main.1ddc2d8d.js"
+  },
+  {
+    "revision": "863a496da0b42b574825d69edd09a0af",
+    "url": "/static/media/control-ps5.863a496d.png"
+  },
+  {
+    "revision": "3eec2cccefe2421ba5df438980c21659",
+    "url": "/static/media/control-xboxOne.3eec2ccc.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "78f46f7f626e58591d291a5928cde614",
+    "url": "/static/media/iphone.78f46f7f.png"
+  },
+  {
+    "revision": "bca0d359e63dbc49d286c8eea2a8c59f",
+    "url": "/static/media/ps5-bg.bca0d359.jpg"
+  },
+  {
+    "revision": "1bdb3240ef11fbf5a00f1cc9a1120ab7",
+    "url": "/static/media/ps5.1bdb3240.png"
+  }
+]);
